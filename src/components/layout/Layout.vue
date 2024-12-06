9d9a398bbd2f34371c19151967b7dabc36ffb544
Farid Mahmudov <razormad666@gmail.com>
2024-12-06 13:49:03 +0300

    update-1

---
+++ src/components/layout/Layout.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+import Header from './layout-partials/Header.vue';
+import Footer from './layout-partials/Footer.vue';
+</script>
+
+
+
+<template>
+    <div id="app-layout">
+
+        <Header />
+
+            <slot name="content" />
+
+        <Footer />
+        
+
+    </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
