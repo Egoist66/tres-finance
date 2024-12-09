7de310fc617af27954cab381f9b6b0f056ac69a9
Farid Mahmudov <razormad666@gmail.com>
2024-12-09 15:55:26 +0300

    update

--- src/utils/delay.ts
+++ src/utils/delay.ts
@@ -1,1 +1,7 @@
+/**
+ * Delay function that returns a promise that resolves after specified amount of time.
+ * @param ms The amount of time to wait in milliseconds
+ * @returns A promise that resolves after the specified amount of time
+ */
+
 export const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
